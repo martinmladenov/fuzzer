a36c84db27cf3dbd1a4f68ab1e46c176441b38cd
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-07-09 16:58:56 +0300

    Add demo

--- src/main/java/io/github/martinmladenov/fuzzer/Main.java
+++ src/main/java/io/github/martinmladenov/fuzzer/Main.java
@@ -1,7 +1,31 @@
 package io.github.martinmladenov.fuzzer;
 
+import io.github.martinmladenov.fuzzer.grammar.base.*;
+import io.github.martinmladenov.fuzzer.grammar.base.ext.*;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var demo = new Concatenate(
+                new Digit(),
+                new AlphaLowercase(),
+                new AlphaUppercase(),
+                new ZeroOrMore(new AlphaLowercase()),
+                new ZeroOrMore(new AlphaUppercase()),
+                new OneOrMore(new AlphaLowercase()),
+                new OneOrMore(new AlphaUppercase())
+        );
+
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random(123123);
+
+        for (int i = 0; i < 100; i++) {
+            sb.append(i).append(": ");
+            demo.generate(sb, rnd);
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
