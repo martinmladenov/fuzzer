571579376c381187ca2789fec15ea46e6ecc2ddd
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-07-09 19:16:53 +0300

    Create URI generation demo

--- src/main/java/io/github/martinmladenov/fuzzer/Main.java
+++ src/main/java/io/github/martinmladenov/fuzzer/Main.java
@@ -2,14 +2,31 @@ package io.github.martinmladenov.fuzzer;
 
 import io.github.martinmladenov.fuzzer.grammar.base.*;
 import io.github.martinmladenov.fuzzer.grammar.base.ext.*;
+import io.github.martinmladenov.fuzzer.grammar.rfc3986.URI;
+import io.github.martinmladenov.fuzzer.grammar.rfc3986.URIReference;
 
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        String s = getDemoStrings(123123);
+//        String s = getDemoStrings(123123);
+        String s = getDemoUris(123123);
 
         System.out.println(s);
+    }
+
+    private static String getDemoUris(int seed) {
+        URIReference symbol = new URIReference();
+
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random(seed);
+
+        for (int i = 0; i < 100; i++) {
+            sb.append(i).append(": ");
+            symbol.generate(sb, rnd);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     private static String getDemoStrings(int seed) {
