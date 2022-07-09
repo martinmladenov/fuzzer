1a01a0fd8790a5f5afec899789f7ed30ed83de65
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-07-09 16:46:28 +0300

    Define a repetition core symbol

---
+++ src/main/java/io/github/martinmladenov/fuzzer/grammar/base/Repeat.java
@@ -0,0 +1,44 @@
+package io.github.martinmladenov.fuzzer.grammar.base;
+
+import java.util.Random;
+
+/**
+ * Generate a certain symbol a random number of times.
+ */
+public class Repeat extends Symbol {
+
+    // TODO: This is an arbitrary value, perhaps it should be changed in the future
+    protected static final int DEFAULT_MAX = 50;
+
+    private final int min;
+    private final int max;
+    private final Symbol symbol;
+
+    /**
+     * @param symbol the symbol to generate
+     * @param min    the minimum number of times to generate the symbol (inclusive)
+     * @param max    the maximum number of times to generate the symbol (exclusive)
+     */
+    public Repeat(Symbol symbol, int min, int max) {
+        if (min < 0) {
+            throw new IllegalArgumentException("min must be >= 0");
+        }
+
+        if (max <= min) {
+            throw new IllegalArgumentException("max must be >= min");
+        }
+
+        this.symbol = symbol;
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public void generate(StringBuilder sb, Random rnd) {
+        int count = rnd.nextInt(max - min) + min;
+        for (int i = 0; i < count; i++) {
+            symbol.generate(sb, rnd);
+        }
+    }
+
+}
