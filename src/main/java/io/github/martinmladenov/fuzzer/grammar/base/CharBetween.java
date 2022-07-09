6e863aa816a352abb0221aac23c18f0f631eef44
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-07-09 18:47:19 +0300

    Add CharBetween base symbol

---
+++ src/main/java/io/github/martinmladenov/fuzzer/grammar/base/CharBetween.java
@@ -0,0 +1,28 @@
+package io.github.martinmladenov.fuzzer.grammar.base;
+
+import java.util.Random;
+
+/**
+ * Generate a random ascii character in a predefined range.
+ */
+public class CharBetween extends BaseSymbol{
+
+    char min;
+    char max;
+
+    public CharBetween(char min, char max){
+        if(min <= max){
+            throw new IllegalArgumentException("min must be < max");
+        }
+
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public void generate(StringBuilder sb, Random rnd) {
+        char c = (char) (rnd.nextInt(max - min + 1) + min);
+        sb.append(c);
+    }
+
+}
