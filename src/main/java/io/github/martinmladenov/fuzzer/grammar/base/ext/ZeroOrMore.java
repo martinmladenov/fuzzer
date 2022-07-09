00c086267b59d256e990210bbfface374a763883
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-07-09 18:19:49 +0300

    Redefine ZeroOrMore to cover edge cases better

--- src/main/java/io/github/martinmladenov/fuzzer/grammar/base/ext/ZeroOrMore.java
+++ src/main/java/io/github/martinmladenov/fuzzer/grammar/base/ext/ZeroOrMore.java
@@ -6,10 +6,10 @@ import io.github.martinmladenov.fuzzer.grammar.base.BaseSymbol;
 /**
  * Generate a certain symbol a random non-negative number of times.
  */
-public class ZeroOrMore extends RepeatRandom {
+public class ZeroOrMore extends Optional {
 
     public ZeroOrMore(BaseSymbol symbol) {
-        super(symbol, 0, RepeatRandom.DEFAULT_MAX);
+        super(new OneOrMore(symbol));
     }
 
 }
