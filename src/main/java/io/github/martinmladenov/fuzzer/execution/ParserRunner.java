24543ed6eb047a4b47c5a38bd6ccea2b03033741
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-10-13 18:26:21 +0200

    Implement ParserRunner

--- src/main/java/io/github/martinmladenov/fuzzer/execution/ParserRunner.java
+++ src/main/java/io/github/martinmladenov/fuzzer/execution/ParserRunner.java
@@ -1,7 +1,10 @@
 package io.github.martinmladenov.fuzzer.execution;
 
+import io.github.martinmladenov.fuzzer.execution.result.RunResult;
+import io.github.martinmladenov.fuzzer.execution.result.RunResultBuilder;
 import io.github.martinmladenov.fuzzer.parsers.ParserAdapter;
 import io.github.martinmladenov.fuzzer.parsers.implementations.JavaURIParser;
+import io.github.martinmladenov.fuzzer.parsers.result.ParserResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,17 @@ public class ParserRunner {
     public ParserRunner(List<ParserAdapter> parsers) {
         this.parsers = new ArrayList<>();
         this.parsers.addAll(parsers);
+    }
+
+    public RunResult run(String input) {
+        RunResultBuilder builder = new RunResultBuilder(parsers.size());
+
+        for (ParserAdapter parser : parsers) {
+            ParserResult parserResult = parser.run(input);
+            builder.logResult(parser.getClass(), parserResult);
+        }
+
+        return builder.build();
     }
 
     public static ParserRunner createDefaultParserRunner() {
