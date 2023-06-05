bd65beac369229d269bcb1dcd194f1788483974b
Martin Mladenov <m.mladenov@student.tudelft.nl>
2023-06-05 11:40:00 +0200

    Add csv

--- src/main/java/io/github/martinmladenov/fuzzer/TestValidity.java
+++ src/main/java/io/github/martinmladenov/fuzzer/TestValidity.java
@@ -4,6 +4,10 @@ import io.github.martinmladenov.fuzzer.grammar.rfc3986.URIReference;
 import io.github.martinmladenov.fuzzer.parsers.ParserAdapter;
 import io.github.martinmladenov.fuzzer.parsers.implementations.JavaURIParser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -12,15 +16,54 @@ public class TestValidity {
     public static void main(String[] args) {
         ParserAcceptanceTester tester = new ParserAcceptanceTester();
         UriGenerator generator = new UriGenerator(123);
-        Set<String> used = new HashSet<>();
 
+        Set<String> allFeatures = new HashSet<>();
 
-        String uri = generator.generate(used);
-        System.out.println(uri);
-        used.forEach(System.out::println);
+        int n = 1200;
 
-        boolean accepted = tester.isAccepted(uri);
-        System.out.println(accepted);
+        ArrayList<Result> results = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            Set<String> used = new HashSet<>();
+            String uri = generator.generate(used);
+            allFeatures.addAll(used);
+            boolean accepted = tester.isAccepted(uri);
+            Result result = new Result(used, accepted);
+            results.add(result);
+        }
+
+        String[] featureArr = allFeatures.stream().sorted().toArray(String[]::new);
+
+        StringBuilder csv = new StringBuilder();
+        csv.append(String.join(",", featureArr) + ",label" + '\n');
+
+        for (Result res : results) {
+            for (String feature : featureArr) {
+                csv.append(res.features.contains(feature) ? '1' : '0');
+                csv.append(',');
+            }
+
+            csv.append(res.label ? '1' : '0');
+
+            csv.append('\n');
+        }
+
+        String csvString = csv.toString();
+        try {
+            Files.writeString(Paths.get("results.csv"), csvString);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static class Result {
+        Set<String> features;
+        boolean label;
+
+        public Result(Set<String> features, boolean label) {
+            this.features = features;
+            this.label = label;
+        }
     }
 
     static class ParserAcceptanceTester {
@@ -39,7 +82,6 @@ public class TestValidity {
                 this.parser.run(uri);
                 return true;
             } catch (Exception e) {
-                e.printStackTrace();
                 return false;
             }
         }
